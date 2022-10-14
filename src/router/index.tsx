57298350210fa206f3aa44f6c732362d286f07b0
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-13 23:44:42 -0300

    Feat - Rotas de Home e Clientes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes as RoutesWrapper } from "react-router-dom";
-import  Home  from "../pages/Home";
+import Cliente from "../pages/Client";
+import Home from "../pages/Home";
+import Team from "../pages/teams";
 
 function Routes() {
   return (
     <RoutesWrapper>
       <Route path="/" element={<Home />} />
+      <Route path="/equipe" element={<Team />} />
+      <Route path="/clientes" element={<Cliente />} />
     </RoutesWrapper>
   );
 }
